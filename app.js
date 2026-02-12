ce8fbf47557df8026f3fd37cb83a1369fb7ca1b5
shalini1920 <shalinij195@gmail.com>
2026-02-12 12:22:20 +0530

    Trigger CI/CD pipeline

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("CI/CD Pipeline Working 🚀");
+  res.send("CI/CD Pipeline Success Incoming 😌🔥");
 });
 
 app.listen(3000, () => {
